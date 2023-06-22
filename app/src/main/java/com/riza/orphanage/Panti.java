c42145e030fcb24453c7f560a21388756c06a4e7
Riza umami <rizasi28@mhs.mdp.ac.id>
2023-06-22 18:53:49 +0700

    update

---
+++ app/src/main/java/com/riza/orphanage/Panti.java
@@ -0,0 +1,58 @@
+package com.riza.orphanage;
+
+public class Panti {
+    private String id;
+    private String nama_panti;
+    private String lokasi;
+    private String tentang_panti;
+    private String telephone;
+    private String link_gambar;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama_panti() {
+        return nama_panti;
+    }
+
+    public void setNama_panti(String nama_panti) {
+        this.nama_panti = nama_panti;
+    }
+
+    public String getLokasi() {
+        return lokasi;
+    }
+
+    public void setLokasi(String lokasi) {
+        this.lokasi = lokasi;
+    }
+
+    public String getTentang_panti() {
+        return tentang_panti;
+    }
+
+    public void setTentang_panti(String tentang_panti) {
+        this.tentang_panti = tentang_panti;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getLink_gambar() {
+        return link_gambar;
+    }
+
+    public void setLink_gambar(String link_gambar) {
+        this.link_gambar = link_gambar;
+    }
+}
